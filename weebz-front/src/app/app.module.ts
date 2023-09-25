949fbd86828c8083f799a2416311fcc7e9fb93e0
gozipizi <quentin.gourdel@ensiie.fr>
2023-09-25 16:51:13 +0200

    creation du footer

--- weebz-front/src/app/app.module.ts
+++ weebz-front/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { CatalogueComponent } from './pages/catalogue/catalogue.component';
@@ -15,11 +16,12 @@ import { ContactFormComponent } from './pages/contact/contact-form/contact-form.
   declarations: [
     AppComponent,
     MainHeaderComponent,
+    MainFooterComponent,
     AccueilComponent,
     BlogComponent,
     CatalogueComponent,
     ContactComponent,
-    ContactFormComponent
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
