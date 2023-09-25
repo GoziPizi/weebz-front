48ea15b3e12d725a42949ad2785b164437caa79c
gozipizi <quentin.gourdel@ensiie.fr>
2023-09-25 16:15:58 +0200

    Creation du routing basique, et des premieres pages (catalogue, blog, accueil)

--- weebz-front/src/app/app-routing.module.ts
+++ weebz-front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'catalogue', component: CatalogueComponent},
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- weebz-front/src/app/app.module.ts
+++ weebz-front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    AccueilComponent,
+    BlogComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule,
