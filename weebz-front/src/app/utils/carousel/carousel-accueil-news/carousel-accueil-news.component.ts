7fb90c204ad216dbb45a5b9319e0e2d80c300848
gozipizi <quentin.gourdel@ensiie.fr>
2023-09-30 23:23:54 +0200

    Ajout du carousel news

--- weebz-front/src/app/utils/carousel/carousel-accueil-news/carousel-accueil-news.component.ts
+++ weebz-front/src/app/utils/carousel/carousel-accueil-news/carousel-accueil-news.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselAccueilNewsComponent implements OnInit {
 
+  carouselItems = [
+    { image: '../../../assets/test-news.png', text: 'Texte 1', link: 'link1' },
+    { image: '../../../assets/test-news.png', text: 'Texte 2', link: 'link2' },
+    { image: '../../../assets/test-news.png', text: 'Texte 2', link: 'link2' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
