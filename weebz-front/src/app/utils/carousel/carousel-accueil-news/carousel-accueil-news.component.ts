92c6dee6134245b9a9e3b7637f482e9f26f55f20
gozipizi <quentin.gourdel@ensiie.fr>
2023-10-14 21:11:35 +0200

    Carousel joli

--- weebz-front/src/app/utils/carousel/carousel-accueil-news/carousel-accueil-news.component.ts
+++ weebz-front/src/app/utils/carousel/carousel-accueil-news/carousel-accueil-news.component.ts
@@ -8,14 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselAccueilNewsComponent implements OnInit {
 
   carouselItems = [
-    { image: '../../../assets/test-news.png', text: 'Texte 1', link: 'link1' },
+    { image: '../../../assets/test-news.png', text: 'Texte 0', link: 'link1' },
+    { image: '../../../assets/test-news.png', text: 'Texte 1', link: 'link2' },
     { image: '../../../assets/test-news.png', text: 'Texte 2', link: 'link2' },
-    { image: '../../../assets/test-news.png', text: 'Texte 2', link: 'link2' },
+    { image: '../../../assets/test-news.png', text: 'Texte 3', link: 'link1' },
+    { image: '../../../assets/test-news.png', text: 'Texte 4', link: 'link2' },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLeftArrowClick() {
+    let old_left = this.carouselItems.shift();
+      if(old_left != undefined) this.carouselItems.push(old_left);
+  }
+
+  onRightArrowClick() {
+    let old_right = this.carouselItems.pop();
+      if(old_right != undefined) this.carouselItems.unshift(old_right);
   }
 
 }
