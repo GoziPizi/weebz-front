5135f14d8926e03bd3947326e957391446262185
gozipizi <quentin.gourdel@ensiie.fr>
2024-01-09 22:23:56 +0100

    remove comments sous artworks

--- weebz-front/src/app/main-footer/main-footer.component.ts
+++ weebz-front/src/app/main-footer/main-footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-footer',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainFooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onNavigate(category: string) {
+    this.router.navigate(['/search'], { queryParams: { type: category } });
   }
 
 }
