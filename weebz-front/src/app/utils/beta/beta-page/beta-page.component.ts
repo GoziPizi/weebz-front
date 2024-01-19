e24eb78c3715b4e90d7df2da94447364466213c4
gozipizi <quentin.gourdel@ensiie.fr>
2024-01-19 23:42:25 +0100

    Modif de la beta, ne peut pas rejoindre quand pas co

--- weebz-front/src/app/utils/beta/beta-page/beta-page.component.ts
+++ weebz-front/src/app/utils/beta/beta-page/beta-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BetaService } from '../beta.service';
+import { ApiHandlerService } from 'src/app/services/api-handler.service';
 
 @Component({
   selector: 'app-beta-page',
@@ -9,17 +10,30 @@ import { BetaService } from '../beta.service';
 export class BetaPageComponent {
 
   betaSubscription: any;
+  loggedInSubscription: any;
+  isLogged: boolean = false;
   isInBeta: boolean = false;
 
   constructor(
-    private betaService: BetaService
+    private betaService: BetaService,
+    private apiHandler: ApiHandlerService
   ) {
     this.betaSubscription = this.betaService.getBetaSubject().subscribe((res: boolean) => {
       this.isInBeta = res;
     });
+    this.loggedInSubscription = this.apiHandler.isLoggedIn.subscribe((res: boolean) => {
+      this.isLogged = res;
+    });
+  }
+
+  ngOnDestroy() {
+    this.betaSubscription.unsubscribe();
   }
 
   onButtonEnroll() {
+    if(!this.isLogged) {
+      return;
+    }
     if (this.isInBeta) {
       this.betaService.leaveBeta();
     }
